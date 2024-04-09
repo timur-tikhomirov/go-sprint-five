859e82043e5e7ce7fb25bc6137ca3dca48338d8b
Timur Tikhomirov <timur.tikh@yandex.ru>
2024-04-10 00:03:17 +0300

    Добавил подсчет калорий

--- main.go
+++ main.go
@@ -206,12 +206,12 @@ func (s Swimming) TrainingInfo() InfoMessage {
 // ReadData возвращает информацию о проведенной тренировке.
 func ReadData(training CaloriesCalculator) string {
 	// получите количество затраченных калорий
-	//calories := training.Calories()
+	calories := training.Calories()
 
 	// получите информацию о тренировке
 	info := training.TrainingInfo()
 	// добавьте полученные калории в структуру с информацией о тренировке
-	//...
+	info.Calories = calories
 
 	return fmt.Sprint(info)
 }
